04fc917f2c0ba9ef5a2472ae30e48d9f6fddac52
exoutia <jhav365@gmail.com>
2025-01-12 10:18:52 +0530

    adding basic editor functionality and now working on toolbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
+import Link from "next/link";
+
 const Home = () => {
   return (
-    <div className="bg-red-50 flex min-h-screen items-center justify-center">
-      <p>hello world</p>
+    <div className="bg-green-50 justify-center flex min-h-screen items-center">
+      Click <Link href={"/documents/123"}><span className="mx-1">Here</span></Link> to go to document id
     </div>
-  );
+  )
 }
-
 export default Home;
